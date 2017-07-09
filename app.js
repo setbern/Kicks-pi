78bd996dc0b1df3ee8d66648ef74bc6c6d66c2ed
Bernardo Garciarivas <brgarciarivas@gmail.com>
2017-07-08 20:47:35 -0400

    test

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const io = require('socket.io-client');
+
+
 console.log('boolin no hooolin')
+
+const socket = io('http:// 10.1.10.152');
+
+io.on('connection', function(socket){
+  console.log('up and down');
+
+
+});
