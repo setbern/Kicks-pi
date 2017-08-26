932039324a9b1a407d40bde6c87667f21a22b70f
brgarciarivas <brgarciarivas@gmail.com>
2017-08-26 16:52:18 -0400

    some sick string interpoliation

--- api/handlers/kick.js
+++ api/handlers/kick.js
@@ -72,7 +72,7 @@ var kick = module.exports = (function() {
                     console.log('i ' +i)
                     console.log(lights[i])
                     params = {
-                        emit: emitter[lights[i]],
+                        emit: emitter[lights[" " + i]],
                         light: i,
                     }
                     console.log('emit params')
