e5d00ff2b23bb3661d3be31d2318bc7bbe1207cd
brgarciarivas <brgarciarivas@gmail.com>
2017-10-29 22:20:58 -0400

    fack

--- api/types/SocketType.js
+++ api/types/SocketType.js
@@ -6,7 +6,7 @@ var SocketType = module.exports = new g.GraphQLInputObjectType({
     fields: function(model) {
         return {
             light:{
-                type: new g.GraphQLNonNull(g.GraphQString)
+                type: new g.GraphQLNonNull(g.GraphQLString)
             },
             status:{
                 type: new g.GraphQLNonNull(g.GraphQLBoolean)
